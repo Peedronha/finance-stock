5bef8adc267eaf92cf5bbd42002600c1888a4110
Pedro Leonardo <pedro.leonardo7738@gmail.com>
2023-06-26 13:03:18 -0300

    detalhes adicionais

--- finance-and-stock-app/src/app/model/user.model.ts
+++ finance-and-stock-app/src/app/model/user.model.ts
@@ -4,14 +4,16 @@ export class User{
   public email: string;
   public password: string;
   public authStatus : string;
+  public company: string;
   public token?: number;
 
-  constructor({ id, email, password, authStatus, token}:
-    { id?: number; email?: string; password?: string; authStatus?: string; token?:number} = {}) {
+  constructor({ id, email, password, authStatus, token, company}:
+    { id?: number; email?: string; password?: string; authStatus?: string; token?:number, company?: string} = {}) {
         this.id = id || 0;
         this.email = email || '';
         this.password = password || '';
         this.authStatus = authStatus || '';
         this.token = id ;
+        this.company = company || '';
     }
 }
