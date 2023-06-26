26c7e95a726d9b4d0bc47e9277bb6fe53f6a0ad5
Pedro Leonardo <pedro.leonardo7738@gmail.com>
2023-06-26 12:58:22 -0300

    add product routing

--- finance-and-stock-app/src/app/components/dashboard/add-product/add-product.component.ts
+++ finance-and-stock-app/src/app/components/dashboard/add-product/add-product.component.ts
@@ -2,34 +2,39 @@ import {Component, OnInit} from '@angular/core';
 import {Product} from "../../../model/product.model";
 import {NgForm} from "@angular/forms";
 import {getCookie} from "typescript-cookie";
+import {Router} from "@angular/router";
+import {ProductService} from "../../../services/product/product.service";
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css']
 })
-export class AddProductComponent implements OnInit{
+export class AddProductComponent implements OnInit {
 
   product: Product | any;
 
   ngOnInit(): void {
 
   }
-  constructor() {
+
+  constructor(private router: Router, private productService: ProductService) {
 
   }
-  submitForm(registerForm: NgForm){
-    // this.homeService.validateRegister(this.customer).subscribe(
-    //
-    //   responseData => {
-    //
-    //     window.sessionStorage.setItem("Authorization",responseData.headers.get('Authorization')!);
-    //     this.customer = <any> responseData.body;
-    //     this.customer.authStatus = 'AUTH';
-    //     window.sessionStorage.setItem("userdetails",JSON.stringify(this.customer));
-    //     let xsrf = getCookie('XSRF-TOKEN')!;
-    //     window.sessionStorage.setItem("XSRF-TOKEN",xsrf);
-    //     this.router.navigate(['dashboard']);
-    //   });
+
+  submitForm(registerForm: NgForm) {
+    //pode ser this.product falta testar
+    this.productService.saveProduct(registerForm).subscribe(
+      responseData => {
+
+        // window.sessionStorage.setItem("Authorization",responseData.headers.get('Authorization')!);
+        // this.customer = <any> responseData.body;
+        // this.customer.authStatus = 'AUTH';
+        // window.sessionStorage.setItem("userdetails",JSON.stringify(this.customer));
+        // let xsrf = getCookie('XSRF-TOKEN')!;
+        // window.sessionStorage.setItem("XSRF-TOKEN",xsrf);
+        // this.router.navigate(['dashboard']);
+      });
   }
 }
+
